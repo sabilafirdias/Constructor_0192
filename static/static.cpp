94b74fd6f578a32a17679132e639c2374585fc51
sabilafiridas <sabilafirdias@gmail.com>
2024-05-14 15:05:01 +0700

    membuat prosedur printAll mahasiswa

--- static/static.cpp
+++ static/static.cpp
@@ -26,5 +26,11 @@ void Mahasiswa::setID() {
     id = ++nim;
 }
 
+void Mahasiswa::printAll() {
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
 
 
