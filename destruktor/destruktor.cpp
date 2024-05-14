539a3399aef20a7ce1b5f8b2c6a707232bfda564
sabilafiridas <sabilafirdias@gmail.com>
2024-05-14 15:58:13 +0700

    membuat prosedur isiData

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -35,6 +35,12 @@ void angka::cetakData() {
     }
 }
 
+void angka::isiData() {
+    for (int i = 1; i <= panjang; i++) {
+        cout << i << " = "; cin >> arr[i];
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
