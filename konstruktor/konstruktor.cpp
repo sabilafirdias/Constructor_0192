8e3e5a8b6bca15a5368cc94bb8da279234d54180
sabilafiridas <sabilafirdias@gmail.com>
2024-05-14 15:33:34 +0700

    membuat objek

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -39,5 +39,15 @@ void mahasiswa::cetak() {
 
 int main()
 {
-    
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Indra");
+    mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+   
 }
