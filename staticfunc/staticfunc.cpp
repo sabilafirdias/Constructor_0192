982d80154bd744777afd9e7ff745372a767a5a8b
sabilafiridas <sabilafirdias@gmail.com>
2024-05-14 15:09:25 +0700

    membuat class mahasiswa

--- staticfunc/staticfunc.cpp
+++ staticfunc/staticfunc.cpp
@@ -6,47 +6,5 @@
 using namespace std;
 
 class mahasiswa {
-private:
-    static int nim;
-public:
-    int id;
-    string nama;
 
-    void setID();
-    void printAll();
-
-    static void setNim(int pNim) { nim = pNim; /*Definisi function*/ }
-    static int getNim() { return nim; /*Definisi function*/ }
-
-    mahasiswa(string pnama) :nama(pnama) { setID(); }
-};
-
-int mahasiswa::nim = 0;
-
-void mahasiswa::setID() {
-    id = ++nim;
-}
-
-void mahasiswa::printAll() {
-    cout << "ID   = " << id << endl;
-    cout << "Nama = " << nama << endl;
-    cout << endl;
-}
-
-int main()
-{
-    mahasiswa mhs1("Sri Dadi");
-    mahasiswa mhs2("Budi Jatmiko");
-    mahasiswa::setNim(9);   //mengakses nilai melalui static member function "setNim"
-    mahasiswa mhs3("Andi Janu");
-    mahasiswa mhs4("Joko Wahono");
-
-    mhs1.printAll();
-    mhs2.printAll();
-    mhs3.printAll();
-    mhs4.printAll();
-
-    cout << "akses dari luar object = " << mahasiswa::getNim() << endl;     //mengakses nim
-    system("pause");
-}
 
