8d3624842bc710bcf70c680d145b733779124a57
sabilafiridas <sabilafirdias@gmail.com>
2024-05-14 15:35:29 +0700

    membuat class buku

---
+++ chainfunction/chainfunction.cpp
@@ -0,0 +1,15 @@
+// chainfunction.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class buku {
+
+};
+
+int main()
+{
+    
+}
+
