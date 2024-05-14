0d9863740d787f5b21d445bdd8b444995ad16fc7
sabilafiridas <sabilafirdias@gmail.com>
2024-05-14 15:48:37 +0700

    membuat class angka

---
+++ destruktor/destruktor.cpp
@@ -0,0 +1,15 @@
+// destruktor.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class angka {
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
